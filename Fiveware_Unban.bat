dff2cc5b6006d73703f1e99326531296995c98a3
weendev <122233442+weendev@users.noreply.github.com>
2023-01-29 03:34:43 +0300

    Add files via upload
    Unban Tool Open Code !

---
+++ Fiveware_Unban.bat
@@ -0,0 +1,10 @@
+@echo off
+title Fiveware Unban Method - ween#1438
+color 0b                                                       
+echo Unban Press Enter!
+echo Not: close epic games and steam or it won't work
+pause
+RD /S /Q C:\Users\%username%\AppData\Local\DigitalEntitlements
+
+
+
